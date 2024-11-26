2a67fcc10ebe01a48517700945ac41642bd148f9
murarii07 <abhayaj07@gmail.com>
2024-11-27 03:35:48 +0530

    first

---
+++ src/Components/App.jsx
@@ -0,0 +1,237 @@
+import { useEffect, useRef, useState } from 'react'
+
+import '../App.css'
+import { players } from '../team'
+
+function App() {
+  const [playersDetails] = useState(players)
+  const [arr, setArr] = useState(playersDetails[0])
+  const [count, setCount] = useState(0)
+  // const countRef=useRef(count)
+  const [price, setPrice] = useState(arr.basePrice.split("")[0])
+  const [teamArr] = useState(["Chennai Super Kings",
+    "Mumbai Indians",
+    "Royal Challengers Bangalore",
+    "Kolkata Knight Riders",
+    "Delhi Capitals",
+    "Punjab Kings",
+    "Rajasthan Royals",
+    "Sunrisers Hyderabad",
+
+    "Lucknow Super Giants",
+    "Gujarat Titans"])
+
+  const [teamDetails,setTeamDetails] = useState([
+    {
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  },{
+    name: "Chennai Super Kings",
+    spent: 0,
+    remaining: 80,
+    WicketKeeper: [],
+    Batsman: [],
+    Finisher: [],
+    Bowlers: [],
+    Allrounders: []
+
+  }])
+  const h = (e) => {
+    setPrice(e.target.value)
+  }
+
+  useEffect(() => {
+    console.log("A")
+  }, [])
+  useEffect(() => {
+    setArr(playersDetails[count])
+  }, [count])
+
+  useEffect(()=>{
+    setPrice(arr.basePrice.split("")[0])
+
+  },[arr])
+
+  return (
+    <>
+      <header className='min-h-16 bg-blue-500 flex justify-center items-center mt-0'>
+        <h1 className=' text-white font-extrabold text-3xl'>IPL 2024 Auction Tracker</h1>
+      </header>
+      <div className=' w-11/12 flex flex-col  gap-10 mx-auto mt-3'>
+        <button className="bg-transparent hover:bg-blue-500 max-w-32  text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded " onClick={() => {
+          // countRef.current++
+          // console.log(countRef.current)
+
+          setCount(s => {
+            if (s >= 0 && s < playersDetails.length) {
+              s += 1
+              return s;
+            }
+            return s
+          })
+          
+        }
+        }>
+          Next Bid
+        </button>
+
+        <div className=' mx-auto w-full border-2 min-h-44 shadow-xl flex flex-wrap gap-14 py-10 px-1 justify-center '>
+          {Object.keys(arr).map((x, index) => (
+            <div className=' flex gap-2 items-center' key={index}>
+              <label htmlFor={x}>{x}&nbsp;:</label>
+              <input type="text" name={x} required className='bg-gray-200 rounded-md p-2 text-gray-600' value={arr[x]} readOnly />
+            </div>
+          ))}
+
+          <div>
+            <label htmlFor="team">Select Team:</label>
+            <select id="team" className='p-2 rounded-md px-3 bg-white border-2' required>
+              {teamArr.map((x, index) =>
+                <option value={x} key={index}>{x}</option>
+              )}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="playerPrice">Price (in Crores):</label>
+            <input type="number" id="playerPrice" required step={(price >= 5 ? 0.25 : 0.20)} min="0" defaultValue={price}  className='rounded-md p-2 border-2' onChange={h} />
+          </div>
+
+
+        </div>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded max-w-36 " onClick={()=>{
+          const d=teamDetails.map((x)=>{
+            if(x.name==="Chennai Super Kings"){
+              console.log(arr.playerName)
+              return {...x,Batsman:[...x.Batsman,`${arr.playerName} (${arr.Rank})- ₹${price}crore`],spent:price, remaining:(x.remaining-price).toFixed(2), }
+            }
+            return x
+          })
+      setTeamDetails(d)
+        }}>
+          ADD player
+        </button>
+
+      </div>
+
+      {/* team bufget */}
+      <div className='mx-auto w-full'>
+        <h1 className='text-center'>Team Budget</h1>
+        <div className='w-10/12 flex flex-wrap mx-auto gap-x-4 justify-evenly mb-4 gap-y-6'>
+          {teamArr.map((x, index) => (
+            <div className='flex flex-col gap-2 items-center  border-2 rounded-md box-border min-w-52 shadow-lg p-2' key={index}>
+              <h3>{x}</h3>
+              {/* {teamDetails[index]} */}
+              <p>Spent: ₹<span id="${team}-spent">{teamDetails[index].spent}</span> crore</p>
+              <p>Remaining: ₹<span id="${team}-remaining">{teamDetails[index].remaining}</span> crore</p>
+              <div id="${team}-Wicketkeeper" ><h4>Wicketkeeper:</h4><ul>{teamDetails[0].WicketKeeper.map((x,index)=>
+              <li key={index}>{x}</li>
+              )}</ul></div>
+              <div id="${team}-Wicketkeeper" ><h4>Bowler:</h4><ul>{teamDetails[0].Bowlers.map((x,index)=>
+              <li key={index}>{x}</li>
+              )}</ul></div>
+              <div id="${team}-Wicketkeeper" ><h4>Batsman:</h4><ul>{teamDetails[0].Batsman.map((x,index)=>
+              <li key={index}>{x}</li>
+              )}</ul></div>
+              <div id="${team}-Wicketkeeper" ><h4>All rounders:</h4><ul>{teamDetails[0].Allrounders.map((x,index)=>
+              <li key={index}>{x}</li>
+              )}</ul></div>
+               <div id="${team}-Wicketkeeper" ><h4> Finisher:</h4><ul>{teamDetails[0].Finisher.map((x,index)=>
+              <li key={index}>{x}</li>
+              )}</ul></div>
+               
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/team.js
@@ -0,0 +1,764 @@
+export const players=[
+  {
+    "playerName": "Ravi Bishnoi",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Sarfaraz Khan",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Deepak Chahar",
+    "Role": "Bowler",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "David Warner",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Hardik Pandya",
+    "Role": "All-rounder",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Andre Russell",
+    "Role": "All-rounder",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "T Natarajan",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Washington Sundar",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Saurabh Tiwary",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Pat Cummins",
+    "Role": "Bowler",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Ashton Agar",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Abhishek Sharma",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Rajat Patidar",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Sai Kishore",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Tim David",
+    "Role": "Finisher",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Gurkeerat Singh Mann",
+    "Role": "Finisher",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Mitchell Santner",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Phil Salt",
+    "Role": "Wicketkeeper",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Devdutt Padikkal",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Kuldeep Sen",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Jonny Bairstow",
+    "Role": "Wicketkeeper",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Kyle Mayers",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Tom Curran",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Priyam Garg",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Umran Malik",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Faf du Plessis",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Kagiso Rabada",
+    "Role": "Bowler",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Kane Williamson",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Mandeep Singh",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Manish Pandey",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Dinesh Karthik",
+    "Role": "Wicketkeeper",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Prasidh Krishna",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Baba Aparajith",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Shubman Gill",
+    "Role": "Batsman",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Harpreet Brar",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Krunal Pandya",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Lasith Malinga",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Cameron Green",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Washington Sundar",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Shreyas Iyer",
+    "Role": "Batsman",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Varun Chakravarthy",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Aiden Markram",
+    "Role": "Batsman",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Devendra Bishoo",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Nicholas Pooran",
+    "Role": "Finisher",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Mohit Sharma",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Sandeep Sharma",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Chris Morris",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Yashasvi Jaiswal",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "KL Rahul",
+    "Role": "Wicketkeeper",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "AB de Villiers",
+    "Role": "Finisher",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Kieron Pollard",
+    "Role": "Finisher",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Alex Hales",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Shikhar Dhawan",
+    "Role": "Batsman",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Ruturaj Gaikwad",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Arshdeep Singh",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Manan Vohra",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Jasprit Bumrah",
+    "Role": "Bowler",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Rahul Tripathi",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Marcus Stoinis",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Harry Brook",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Mustafizur Rahman",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Kuldeep Yadav",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Shahrukh Khan",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Akash Deep",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Sai Sudharsan",
+    "Role": "Batsman",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Shardul Thakur",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Mukesh Choudhary",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Nicholas Pooran",
+    "Role": "Wicketkeeper",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Mayank Agarwal",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Evin Lewis",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Jitesh Sharma",
+    "Role": "Wicketkeeper",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Virat Kohli",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Lungi Ngidi",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "KS Bharat",
+    "Role": "Wicketkeeper",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Jos Buttler",
+    "Role": "Wicketkeeper",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Moeen Ali",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Wriddhiman Saha",
+    "Role": "Wicketkeeper",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Shivam Dube",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Deepak Hooda",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Jason Holder",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Chris Jordan",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Rohit Sharma",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Mitchell Starc",
+    "Role": "Bowler",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Rahul Tewatia",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Shakib Al Hasan",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Anuj Rawat",
+    "Role": "Wicketkeeper",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Chris Woakes",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Ishan Kishan",
+    "Role": "Wicketkeeper",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Rahmanullah Gurbaz",
+    "Role": "Wicketkeeper",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "David Miller",
+    "Role": "Finisher",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Ravindra Jadeja",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Khaleel Ahmed",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Mohammed Shami",
+    "Role": "Bowler",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Vijay Shankar",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Rishi Dhawan",
+    "Role": "All-rounder",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Suryakumar Yadav",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Ben Stokes",
+    "Role": "All-rounder",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Yuzvendra Chahal",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Chetan Sakariya",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Alex Carey",
+    "Role": "Wicketkeeper",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Mohammad Nabi",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Shivam Mavi",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Ajinkya Rahane",
+    "Role": "Batsman",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Harshal Patel",
+    "Role": "Finisher",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Marco Jansen",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Glenn Maxwell",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Tim Southee",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Adam Zampa",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Kartik Tyagi",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Siddarth Kaul",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Rishabh Pant",
+    "Role": "Wicketkeeper",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Lockie Ferguson",
+    "Role": "Bowler",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Sanju Samson",
+    "Role": "Wicketkeeper",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Shimron Hetmyer",
+    "Role": "Finisher",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Quinton de Kock",
+    "Role": "Wicketkeeper",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Anrich Nortje",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Avesh Khan",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Rassie van der Dussen",
+    "Role": "Batsman",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "MS Dhoni",
+    "Role": "Finisher",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Rahul Chahar",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Riley Meredith",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Jaydev Unadkat",
+    "Role": "Bowler",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Liam Livingstone",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  },
+  {
+    "playerName": "Steve Smith",
+    "Role": "Batsman",
+    "Rank": "A++",
+    "basePrice": "5 crore"
+  },
+  {
+    "playerName": "Venkatesh Iyer",
+    "Role": "All-rounder",
+    "Rank": "B",
+    "basePrice": "2 crore"
+  },
+  {
+    "playerName": "Wanindu Hasaranga",
+    "Role": "Bowler",
+    "Rank": "A",
+    "basePrice": "3 crore"
+  },
+  {
+    "playerName": "Sam Curran",
+    "Role": "All-rounder",
+    "Rank": "A+",
+    "basePrice": "4 crore"
+  }
+]
