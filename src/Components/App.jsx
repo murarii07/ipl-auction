0d352629a825b595879a6e7a4829fcdb283985d1
Murariji <102669235+murarii07@users.noreply.github.com>
2024-11-27 14:59:03 +0530

    c

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -96,11 +96,11 @@ function App() {
 
 
         </div>
-        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded max-w-36 " onClick={() => {
-          const d = teamDetails.map((x) => {
-            if (x.name === t) {
-              console.log("s", x[arr.Role])
-              return { ...x, [`${arr.Role}`]: [...x[`${arr.Role}`], `${arr.playerName} (${arr.Rank})- ₹${price}crore`], spent: price, remaining: (x.remaining - price).toFixed(2), }
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded max-w-36 " onClick={()=>{
+          const d=teamDetails.map((x)=>{
+            if(x.name===t){
+              console.log("s",x[arr.Role])
+                          return {...x,[`${arr.Role}`]:[...x[`${arr.Role}`],`${arr.playerName} (${arr.Rank})- ₹${price}crore`],spent:price, remaining:(x.remaining-price).toFixed(2), }
             }
             return x
           })
