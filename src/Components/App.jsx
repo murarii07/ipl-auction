950f97503a303d470649ab77631cb715cb0d3f98
Murariji <102669235+murarii07@users.noreply.github.com>
2024-11-27 14:24:13 +0530

    res

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -95,7 +95,7 @@ function App() {
         <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded max-w-36 " onClick={()=>{
           const d=teamDetails.map((x)=>{
             if(x.name==="Chennai Super Kings"){
-              console.log(arr.Role)
+              console.log("s",x[arr.Role])
               let o={}
               o[arr.Role]=[...x[arr.Role],(`${arr.playerName} (${arr.Rank})- ₹${price}crore`)]
               console.log(o)
