44bfaaa724abba8f082d6bbf08e4b8f0386091eb
Murariji <102669235+murarii07@users.noreply.github.com>
2024-11-27 13:35:38 +0530

    reutrn updated

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -111,25 +111,25 @@ function App() {
       <div className='mx-auto w-full'>
         <h1 className='text-center'>Team Budget</h1>
         <div className='w-10/12 flex flex-wrap mx-auto gap-x-4 justify-evenly mb-4 gap-y-6'>
-          {teamArr.map((x, index) => (
+          {teamDetails.map((x, index) => (
             <div className='flex flex-col gap-2 items-center  border-2 rounded-md box-border min-w-52 shadow-lg p-2' key={index}>
-              <h3>{x}</h3>
+              <h3>{x.name}</h3>
               {/* {teamDetails[index]} */}
-              <p>Spent: ₹<span id="${team}-spent">{teamDetails[index].spent}</span> crore</p>
-              <p>Remaining: ₹<span id="${team}-remaining">{teamDetails[index].remaining}</span> crore</p>
-              <div id="${team}-Wicketkeeper" ><h4>Wicketkeeper:</h4><ul>{teamDetails[0].WicketKeeper.map((x,index)=>
+              <p>Spent: ₹<span id="${team}-spent">{x.spent}</span> crore</p>
+              <p>Remaining: ₹<span id="${team}-remaining">{x.remaining}</span> crore</p>
+              <div id="${team}-Wicketkeeper" ><h4>Wicketkeeper:</h4><ul>{x.WicketKeeper.map((x,index)=>
               <li key={index}>{x}</li>
               )}</ul></div>
-              <div id="${team}-Wicketkeeper" ><h4>Bowler:</h4><ul>{teamDetails[0].Bowlers.map((x,index)=>
+              <div id="${team}-Wicketkeeper" ><h4>Bowler:</h4><ul>{x.Bowlers.map((x,index)=>
               <li key={index}>{x}</li>
               )}</ul></div>
-              <div id="${team}-Wicketkeeper" ><h4>Batsman:</h4><ul>{teamDetails[0].Batsman.map((x,index)=>
+              <div id="${team}-Wicketkeeper" ><h4>Batsman:</h4><ul>{x.Batsman.map((x,index)=>
               <li key={index}>{x}</li>
               )}</ul></div>
-              <div id="${team}-Wicketkeeper" ><h4>All rounders:</h4><ul>{teamDetails[0].Allrounders.map((x,index)=>
+              <div id="${team}-Wicketkeeper" ><h4>All rounders:</h4><ul>{x.Allrounders.map((x,index)=>
               <li key={index}>{x}</li>
               )}</ul></div>
-               <div id="${team}-Wicketkeeper" ><h4> Finisher:</h4><ul>{teamDetails[0].Finisher.map((x,index)=>
+               <div id="${team}-Wicketkeeper" ><h4> Finisher:</h4><ul>{x.Finisher.map((x,index)=>
               <li key={index}>{x}</li>
               )}</ul></div>
                
